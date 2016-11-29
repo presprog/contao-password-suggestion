630cfd83579d7e1449737a591af49367a799467c
Benedict Zinke <bz@presentprogressive.de>
2016-11-29 10:32:04 +0100

    add assets in config.php (fix #4)

--- src/config/config.php
+++ src/config/config.php
@@ -3,3 +3,14 @@
 $GLOBALS['TL_HOOKS']['parseWidget'][] = [
     'PwSuggestion', 'enhancePasswordWidget'
 ];
+
+// add assets
+if (TL_MODE == 'BE') {
+    if (Config::get('debugMode')) {
+        $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/password-suggestion/assets/pws.js';
+        $GLOBALS['TL_CSS'][] = 'system/modules/password-suggestion/assets/pws.css';
+    } else {
+        $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/password-suggestion/assets/pws.min.js';
+        $GLOBALS['TL_CSS'][] = 'system/modules/password-suggestion/assets/pws.min.css';
+    }
+}
